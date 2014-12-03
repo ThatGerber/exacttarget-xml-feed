e77bffac4e90a5c1ced5c682efb9c6ad7d7efb0f
Christopher Gerber <chriswgerber@gmail.com>
2014-12-03 17:21:43 -0600

    Removed extra spacing and returns from item template.

--- templates/item-template.php
+++ templates/item-template.php
@@ -1,17 +1,9 @@
 <?php global $post; ?>
 <item>
 	<?php rss_enclosure(); ?>
-	<title>
-		<?php the_title_rss() ?>
-	</title>
-	<link>
-	<?php the_permalink_rss() ?>
-	</link>
-	<image>
-		<?php if ( has_post_thumbnail() ) {
-			XT_XML_Feed::feed_image( $post, XT_XML_Feed::$cat->slug );
-		} ?>
-	</image>
+	<title><?php the_title_rss() ?></title>
+	<link><?php the_permalink_rss() ?></link>
+	<image><?php if ( has_post_thumbnail() ) { echo XT_XML_Feed::get_feed_image( $post, XT_XML_Feed::$cat->slug ); } ?></image>
 	<description>
 		<![CDATA[
 			<?php XT_XML_Feed::the_description(); ?>
